8ea1dffe0f1288ab25fc791c0aab105995cae7a7
Oleg Grigoryev <baradist@ya.ru>
2020-03-24 19:47:08 +0300

    get env from scenario

--- experiments/train.py
+++ experiments/train.py
@@ -57,9 +57,9 @@ def make_env(scenario_name, arglist, benchmark=False):
     world = scenario.make_world()
     # create multiagent environment
     if benchmark:
-        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, scenario.benchmark_data)
+        env = scenario.get_env(world, scenario.reset_world, scenario.reward, scenario.observation, scenario.benchmark_data)
     else:
-        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation)
+        env = scenario.get_env(world, scenario.reset_world, scenario.reward, scenario.observation)
     return env
 
 def get_trainers(env, num_adversaries, obs_shape_n, arglist):
