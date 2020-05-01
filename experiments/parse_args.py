1d167ed72a5c2481daefc0f7f699671d7c8640de
Oleg_Grigoryev <oleg_grigoryev@epam.com>
2020-05-01 22:43:45 +0300

    Extract args parser to separate file

---
+++ experiments/parse_args.py
@@ -0,0 +1,28 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
+    # Environment
+    parser.add_argument("--scenario", type=str, default="simple_speaker_listener", help="name of the scenario script")
+    parser.add_argument("--max-episode-len", type=int, default=25, help="maximum episode length")
+    parser.add_argument("--num-episodes", type=int, default=100000, help="number of episodes")
+    parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
+    parser.add_argument("--good-policy", type=str, default="maddpg", help="policy for good agents")
+    parser.add_argument("--adv-policy", type=str, default="maddpg", help="policy of adversaries")
+    # Core training parameters
+    parser.add_argument("--lr", type=float, default=1e-2, help="learning rate for Adam optimizer")
+    parser.add_argument("--gamma", type=float, default=0.99, help="discount factor")
+    parser.add_argument("--batch-size", type=int, default=1024, help="number of episodes to optimize at the same time")
+    parser.add_argument("--num-units", type=int, default=64, help="number of units in the mlp")
+    # Checkpointing
+    parser.add_argument("--exp-name", type=str, default=None, help="name of the experiment")
+    parser.add_argument("--save-dir", type=str, default="./out/", help="directory in which training state and model should be saved")
+    parser.add_argument("--save-rate", type=int, default=1000, help="save model once every time this many episodes are completed")
+    parser.add_argument("--load-dir", type=str, default="", help="directory in which training state and model are loaded")
+    # Evaluation
+    parser.add_argument("--display", action="store_true", default=False)
+    parser.add_argument("--benchmark", action="store_true", default=False)
+    parser.add_argument("--benchmark-iters", type=int, default=100000, help="number of iterations run for benchmarking")
+    parser.add_argument("--benchmark-dir", type=str, default="./out/benchmark_files/", help="directory where benchmark data is saved")
+    parser.add_argument("--plots-dir", type=str, default="./out/learning_curves/", help="directory where plot data is saved")
+    return parser.parse_args()
