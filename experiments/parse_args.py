551c8155549d381a4858879072a55b43f0a0f9ba
Oleg_Grigoryev <oleg_grigoryev@epam.com>
2020-06-14 17:04:48 +0300

    ST 4vs2 SB

--- experiments/parse_args.py
+++ experiments/parse_args.py
@@ -6,7 +6,7 @@ def parse_args():
     parser.add_argument("--scenario", type=str, default="simple_tag", help="name of the scenario script")
     parser.add_argument("--max-episode-len", type=int, default=150, help="maximum episode length")
     parser.add_argument("--num-episodes", type=int, default=50000, help="number of episodes")
-    parser.add_argument("--exp-name", type=str, default="st-4vs2-0", help="name of the experiment")
+    parser.add_argument("--exp-name", type=str, default="st-4vs2-sb-0", help="name of the experiment")
     parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
     parser.add_argument("--good-policy", type=str, default="maddpg", help="policy for good agents")
     parser.add_argument("--adv-policy", type=str, default="maddpg", help="policy of adversaries")
